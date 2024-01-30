45bfb9be0b5d9e0d446d875227102b1879d0356f
TBG101 <ziedhrz@gmail.com>
2024-01-30 16:46:27 +0100

    changes to requirements

--- app.py
+++ app.py
@@ -232,4 +232,4 @@ def handle_create_connection(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="192.168.1.13", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
